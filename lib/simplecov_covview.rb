350906fa47c616717a788a4865b1a65639ff89ef
icm7216 <icm7216d@gmail.com>
2021-08-19 15:22:29 +0900

    Add SimplecovCovview::CovView::Resultfile

--- lib/simplecov_covview.rb
+++ lib/simplecov_covview.rb
@@ -2,7 +2,7 @@
 
 require_relative "simplecov_covview/version"
 require_relative "simplecov_covview/formatter"
-
+require_relative "simplecov_covview/resultfile.rb"
 module SimpleCov
   module Formatter
     class CovView
---
+++ lib/simplecov_covview/resultfile.rb
@@ -0,0 +1,40 @@
+# The Resultfile class stores source file's information.
+#
+# @param [Hash] file SimpleCov::SourceFile
+module SimplecovCovview
+  class CovView
+    class Resultfile
+
+      def initialize(file)
+        @file = file
+      end
+
+      def covered_count
+        @file.covered_lines.count
+      end
+
+      def missed_count
+        @file.missed_lines.count
+      end
+
+      def line_digit
+        @file.lines.count.to_s.size
+      end
+
+      def cov_digit
+        @file.coverage_data["lines"].map(&:to_i).max.to_s.size
+      end
+
+      def status
+        {
+          filename: File.basename(@file.filename),
+          line_digit: line_digit,
+          cov_digit: cov_digit,
+          covered: covered_count,
+          missed: missed_count,
+        }
+      end
+
+    end
+  end
+end
